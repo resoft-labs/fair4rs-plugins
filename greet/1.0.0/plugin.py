b6923ec00ca015d5a077d1f29d5c0954627dcb43
Faruk D. <fdiblen@universe>
2025-03-28 16:30:16 +0100

    add a simple greet plugin

---
+++ greet/1.0.0/plugin.py
@@ -0,0 +1,8 @@
+# https://github.com/resoft-labs/fair4rs-plugins/blob/main/greet/1.0.0/plugin.py
+
+def run(args):
+    """
+    Simple greeting plugin example
+    """
+    print("Hello from the greet plugin!")
+
